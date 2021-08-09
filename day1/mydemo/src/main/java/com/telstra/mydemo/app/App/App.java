f8232c6b2ac9d923d30e2f4c52e43c6b91f8dad5
srmono <bvsrao91@gmail.com>
2021-08-09 19:41:23 +0530

    day 1 maven and git

---
+++ day1/mydemo/src/main/java/com/telstra/mydemo/app/App/App.java
@@ -0,0 +1,13 @@
+package com.telstra.mydemo.app.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
