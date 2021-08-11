dd6f29a7f75a58b25087d0adfd47d256a6e3fbb8
srmono <bvsrao91@gmail.com>
2021-08-11 21:16:20 +0530

    IOC context spring di

---
+++ Day3/springcore/src/main/java/com/springcore/ci/Cert.java
@@ -0,0 +1,15 @@
+package com.springcore.ci;
+
+public class Cert {
+	String name;
+
+	public Cert(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
---
+++ Day3/springcore/src/main/java/com/springcore/ci/Person.java
@@ -0,0 +1,20 @@
+package com.springcore.ci;
+
+public class Person {
+	private String name;
+	private int personId;
+	private Cert cert;
+	
+	
+	public Person(String name, int personId, Cert cert) {
+		this.name = name;
+		this.personId = personId;
+		this.cert = cert;
+	}
+
+	@Override
+	public String toString() {
+		return this.name + " : " + this.personId + "{"+ this.cert.name +"}";
+	}
+	
+}
