4f1026125b7a4872c1a1e649f57e9ac8644fa926
srmono <bvsrao91@gmail.com>
2021-08-17 13:10:57 +0530

    spring boot update

--- Day6/Spring-boot-project/src/main/java/com/telstra/springboot/proj/controller/DepartmentController.java
+++ Day6/Spring-boot-project/src/main/java/com/telstra/springboot/proj/controller/DepartmentController.java
@@ -1,8 +1,58 @@
 package com.telstra.springboot.proj.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.telstra.springboot.proj.entity.Department;
+import com.telstra.springboot.proj.error.DepartmentNotFoundException;
+import com.telstra.springboot.proj.service.DepartmentService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class DepartmentController {
 
+    @Autowired
+    private DepartmentService departmentService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside the save method");
+        return  departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside the get all dep's method");
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
+        LOGGER.info("Inside the get dep by id method");
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        LOGGER.info("Inside the delete dep by id api");
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted Sucessfully";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId, department);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
+    }
 }
--- Day6/Spring-boot-project/src/main/java/com/telstra/springboot/proj/service/DepartmentService.java
+++ Day6/Spring-boot-project/src/main/java/com/telstra/springboot/proj/service/DepartmentService.java
@@ -1,5 +1,21 @@
 package com.telstra.springboot.proj.service;
 
+import com.telstra.springboot.proj.entity.Department;
+import com.telstra.springboot.proj.error.DepartmentNotFoundException;
+
+import java.util.List;
+
 public interface DepartmentService {
 
+    public  Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
 }
