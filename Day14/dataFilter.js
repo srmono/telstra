19efcf9729edb8ebbf3195720da7acec394bb610
srmono <bvsrao91@gmail.com>
2021-08-26 15:08:54 +0530

    data filters updated

--- Day14/dataFilter.js
+++ Day14/dataFilter.js
@@ -10,4 +10,73 @@ const projects = [
     { name: "Project Nine", category: "Retail", start: 1971, end: 1989}
 ]
 
-const grades = [23,45,78,92,34,56,98,45,68,49]
+const grades = [23,45,78,92,34,56,98,45,68,49];
+
+
+// for ( let i = 0; i < projects.length; i++){
+//     console.log(projects[i])
+// }
+
+// foreach
+//projects.forEach( proj => console.log(proj))
+
+/// Filter 
+
+//let eligble = [];
+
+// for( let i = 0; i < grades.length; i++ ){
+//     if(grades[i] >= 60){
+//         eligble.push(grades[i])
+//     }
+// }
+
+// let eligible = grades.filter(grade => grade >= 60);
+
+// console.log(eligible)
+
+// get Ecom projects
+
+const ecomProjects = projects.filter( project => project.category === "Ecom");
+
+// Get projects started between 1980 to 1990
+const project90 = projects.filter( project => project.start > 1980 && project.start < 1990);
+
+const projectLast10 = projects.filter(project => project.end - project.start <= 10);
+
+//map()
+let projectNames = projects.map( project => project.name); 
+
+let projectDetails = projects.map( project => `${project.name} started in ${project.start}`)
+
+
+const upgrade = grades.map( grade => grade + 5);
+
+///
+// const sortedProjects = projects.sort(function(p1, p2){
+//     if(p1.start < p2.start){
+//         return 1
+//     } else {
+//         return -1
+//     }
+// })
+
+const sortedProjects = projects.sort( (p1, p2) => ( p1.start < p2.start ? 1 : -1 ) )
+
+const sortedGrades = grades.sort( (a, b) => b - a )
+
+// reduce 
+
+const totalGrades = grades.reduce( (total, grade) => total + grade, 0 )
+
+// total spent
+
+const totalDuration = projects.reduce( (total, project) => total +  (project.end - project.start), 0)
+
+
+//upgrade then filter then sort 
+
+const mergeFunc = grades
+    .map(grade => grade + 5)
+    .filter(grade => grade >= 60)
+    .sort((a, b) => b - a)
+    
